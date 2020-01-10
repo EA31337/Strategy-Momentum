97b8281590ffbdabb74a6596f539fb1f880c5df3
kenorb <kenorb@users.noreply.github.com>
2020-01-10 18:00:31 +0000

    Updates strategy code up to latest standard

---
+++ sets/EURUSD_M30.h
@@ -0,0 +1,26 @@
+//+------------------------------------------------------------------+
+//|                  EA31337 - multi-strategy advanced trading robot |
+//|                       Copyright 2016-2020, 31337 Investments Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+// Defines strategy's parameter values for the given pair symbol and timeframe.
+struct Stg_Momentum_EURUSD_M30_Params : Stg_Momentum_Params {
+  Stg_Momentum_EURUSD_M30_Params() {
+    symbol = "EURUSD";
+    tf = PERIOD_M30;
+    Momentum_Period = 2;
+    Momentum_Applied_Price = 3;
+    Momentum_Shift = 0;
+    Momentum_TrailingStopMethod = 6;
+    Momentum_TrailingProfitMethod = 11;
+    Momentum_SignalOpenLevel = 36;
+    Momentum_SignalBaseMethod = 0;
+    Momentum_SignalOpenMethod1 = 195;
+    Momentum_SignalOpenMethod2 = 0;
+    Momentum_SignalCloseLevel = 36;
+    Momentum_SignalCloseMethod1 = 1;
+    Momentum_SignalCloseMethod2 = 0;
+    Momentum_MaxSpread = 5;
+  }
+};
